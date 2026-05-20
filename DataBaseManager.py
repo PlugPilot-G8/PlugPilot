524c4807e08c327c6a19e94ccf8e18d61f9beb8f
AMSDAF <amsaf@cesar.school>
2026-05-20 20:49:36 -0300

    Estrutura do Sistema

---
+++ DataBaseManager.py
@@ -0,0 +1,4 @@
+import json
+
+def criar_database():
+    print("Criando Banco de Dados...")
---
+++ main.py
@@ -0,0 +1,10 @@
+# Inicializa o sistema
+
+from DataBaseManager import criar_database
+
+def inicializar_sistema():
+    print("Sistema inicializado com sucesso!")
+
+if __name__ == "__main__":
+    print("Inicializando Sistema...")
+    inicializar_sistema()
