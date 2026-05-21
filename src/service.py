b2e93e849e790bbf067377fe07b3822dc6c1f993
LSP4-EXVI <lsp4@cesar.school>
2026-05-21 15:38:04 -0300

    estruturando o service.py CRUD carregadores

--- src/service.py
+++ src/service.py
@@ -1,1 +1,41 @@
 # service.py - Responsável por fornecer as funções de serviço para o sistema, como login, cadastro, controle de unidades e dispositivos.
+
+# Responsavel por gerenciar os carregadores (CRUD)
+
+chargers = {}
+
+# Função responsável por criar um novo carregador
+def criar_carregador():
+    print("criando carregador...")
+
+# Função responsável por exibir o carregador
+def visualizar_carregador():
+    print("exibindo carregador...")
+
+# Função responsável por editar o carregador
+def editar_carregador():
+    print("editando carregador...")
+
+# Função resposável por deletar o carregador
+def deletar_carregador():
+    print("deletando carregador...")
+
+# Responsavel por gerenciar os carregadores (CRUD)
+
+unit = {}
+
+# Função responsável por criar um novo carregador
+def criar_unidade():
+    print("criando unidade...")
+
+# Função responsável por exibir o carregador
+def visualizar_unidade():
+    print("exibindo unidade...")
+
+# Função responsável por editar o carregador
+def editar_unidade():
+    print("editando unidade...")
+
+# Função resposável por deletar o carregador
+def deletar_unidade():
+    print("deletando unidade...")
